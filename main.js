417361fc831a936657ef635af3a92de8aa8f6611
Angela Vogler <angelavogler4@gmail.com>
2017-08-03 15:02:30 -0400

    finished project

--- main.js
+++ main.js
@@ -5,15 +5,47 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
+// create variables assigning set numerical value to J, Q, K
 
 function handValue (hand) {
+//create variable to hold total
+  let total = 0;
+// create for loop to iterate through hand array
+  let cards = [];
+// create variable to turn cards into a numbered array
+for (let i = 0; i < hand.length; i++) {
+  // turn cards into a numbered array
+  if ((hand[i] === "J") || (hand[i] === "Q") || (hand[i] === "K")) {
+    let cardValue = Number("10");
+    cards.push(cardValue);
+  } else if ((hand[i] === "A")) {
+    cards.push(hand[i])
+  } else {
+    let cardValue = Number(hand[i]);
+    cards.push(cardValue);
+  }
+  cards.sort();
+}
 
-
-  return;
+// A either equals 11 OR 1
+for (let i = 0; i < cards.length; i++) {
+  // create if statement to run if A is present
+  if (cards[i] === "A") {
+      if (total <= 10) {
+        total += 11;
+      } else {
+        total += 1;
+      }
+  // do this if A is not present
+  } else {
+    total += cards[i];
+  }
+}
+  return total;
 }
 
 
